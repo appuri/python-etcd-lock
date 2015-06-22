5423ac323597dceb4c8340d74fa3966760fe4931
Nathan Black <nathan@nathanblack.org>
2015-06-22 14:32:22 -0700

    Add a renew method and timer to renew while the lock is acquired. Flushed out the single lock tests.

--- etcdlock/lock.py
+++ etcdlock/lock.py
@@ -1,5 +1,6 @@
 import etcd
 import uuid
+from threading import Timer
 
 class Lock(object):
 
@@ -7,28 +8,36 @@ class Lock(object):
     Lock object using etcd keys to atomically compare and swap.
     """
 
-    def __init__(self, client, key, ttl=60):
+    def __init__(self, client, key, ttl=60, renewSecondsPrior=5, name="anon"):
         """
         Initialize a lock object.
         Args:
             client (Client):  etcd client to use for communication.
             key (string):  key to lock.
             ttl (int):  ttl (in seconds) for the lock to live. Defaults to 60 seconds.
-            refresh (int): time before the end of the ttl period to refresh the lock. Defaults to 5 seconds.
+            renewSecondsPrior (int): time before the end of the ttl period to renew the lock. Defaults to 5 seconds.
         """
-        if ttl is None or ttl is 0:
-            raise ValueError("A positive TTL must be specified")
-        if key is None or key is '':
-            raise ValueError("A etcd key must be specified")
         if not isinstance(client, etcd.Client):
             raise ValueError("A python-etcd Client must be provided")
+        if key is None or key is '':
+            raise ValueError("A etcd key must be specified")
+        if ttl is None or ttl is 0:
+            raise ValueError("A positive TTL must be specified")
+        if renewSecondsPrior is not None:
+            if not isinstance(renewSecondsPrior, int) or renewSecondsPrior < 0:
+                raise ValueError("A positive prior renew must be specified, or None to not renew")
+            if ttl - renewSecondsPrior < 1:
+                raise ValueError("The renew prior time is too close to initial aquisition time - hold the lock for at least 2 seconds")
 
         self.client = client
         if not key.startswith('/'):
             key = '/' + key
         self.key = key
         self.ttl = ttl
+        self.renewSecondsPrior = renewSecondsPrior
         self._index = None
+        self.token = None
+        self.name = name
 
     def __enter__(self):
         self.acquire()
@@ -36,24 +45,77 @@ class Lock(object):
     def __exit__(self, type, value, traceback):
         self.release()
 
-    def acquire(self):
+    def acquire(self, timeout=None):
+        """
+        Aquire the lock. Returns True if the lock was aquired; False otherwise.
+
+        timeout (int): Timeout to wait for the lock to change if it is already acquired.
+            Defaults to None, which will block and retry until acquired. 
+        """
         token = str(uuid.uuid4())
-        try:
-            self.client.test_and_set(self.key, token, "0", ttl=self.ttl)
-            self.token = token
-        except etcd.EtcdKeyNotFound, e:
+        while self.token is None:
             try:
-                self.client.write(self.key, token, prevExist=False, ttl=self.ttl)
+                self.client.test_and_set(self.key, token, "0", ttl=self.ttl)
+                print "%s: lock was available and aquired" % self.name
                 self.token = token
-            except etcd.EtcdAlreadyExist, e:
-                # someone created the right before us
-                self.acquire()
-        except ValueError, e:
-            print e
+            except etcd.EtcdKeyNotFound, e:
+                try:
+                    print "%s: lock didn't exist, aquiring" % self.name
+                    self.client.write(self.key, token, prevExist=False, ttl=self.ttl)
+                    print "%s: -- aquired" % self.name
+                    self.token = token
+                except etcd.EtcdAlreadyExist, e:
+                    print "%s: xx failed" % self.name
+                    pass # someone created the right before us
+            except ValueError, e:
+                # someone else has the lock
+                print "%s: waiting for lock failed" % self.name
+                if False:
+                    self.client.watch(self.key, timeout=timeout)
+                else:
+                    self.client.watch(self.key)
+                print "%s: -- lock changed" % self.name
+
+                if (timeout is not None):
+                    return False
+                else:
+                    print "%s: -- attempting again" % self.name
+
+        if self.renewSecondsPrior is not None:
+            def renew():
+                if (self.renew()):
+                    Timer(self.ttl, self.renew)
+            Timer(self.ttl - self.renewSecondsPrior, lambda: self.renew())
+
+        return True
+
+    def renew(self):
+        """
+        Renew the lock if acquired.
+        """
+        if (self.token is not None):
+            try:
+                self.client.test_and_set(self.key, self.token, self.token, ttl=self.ttl)
+                return True
+            except ValueError, e:
+                self.token = None
+                return False
+
+    def is_locked(self):
+        """
+        Return True if the lock has been and is currently acquired; False otherwise.
+        """
+        return self.token is not None
 
     def release(self):
-        if (self.token != None):
+        """
+        Release the lock if acquired.
+        """
+        # TODO: thread safety (currently the lock may be acquired for one more TTL length)
+        if (self.token is not None):
             try:
                 self.client.test_and_set(self.key, 0, self.token)
             except (ValueError, etcd.EtcdKeyError, etcd.EtcdKeyNotFound) as e:
                 pass # the key already expired or got acquired by someone else
+            finally:
+                self.token = None
--- tests/lock_tests.py
+++ tests/lock_tests.py
@@ -12,10 +12,10 @@ import random
 import time
 from threading import Thread
 
-class EtcdLockTests(unittest.TestCase):
+class SingleLockTests(unittest.TestCase):
     def setUp(self):
         self.client = etcd.Client()
-        self.lock = Lock(self.client, "lock-%d" % random.uniform(1,100000))
+        self.lock = Lock(self.client, "lock-%d" % random.uniform(1,100000), ttl=2, renewSecondsPrior=1)
 
     def teardown(self):
         if (self.lock != None and self.lock.token != None):
@@ -27,33 +27,72 @@ class EtcdLockTests(unittest.TestCase):
     def test_it_should_require_a_valid_ttl(self):
         expect(lambda: Lock(self.client, 'lock/foo', ttl=0)).to.raise_error(ValueError)
 
+    def test_it_should_require_a_valid_renew_prior_time(self):
+        expect(lambda: Lock(self.client, 'lock/foo', renewSecondsPrior=-5)).to.raise_error(ValueError)
+
+    def test_it_should_require_renew_prior_to_be_less_than_the_ttl(self):
+        expect(lambda: Lock(self.client, 'lock/foo', ttl=20, renewSecondsPrior=20)).to.raise_error(ValueError)
+
     def test_it_should_require_an_etcd_key(self):
         expect(lambda: Lock(self.client, key=None)).to.raise_error(ValueError)
 
     def test_it_should_be_able_to_be_acquired_and_released(self):
+        self.lock = Lock(self.client, "lock-%d" % random.uniform(1,100000))
         self.lock.acquire()
         node = self.client.get(self.lock.key)
-        print node
         expect(node.value).to.eq(self.lock.token)
         expect(node.ttl).to.eq(60)
         self.lock.release()
         expect(self.client.get(self.lock.key).value).to.eq("0")
 
-    def test_it_should_not_aquire_a_lock_already_acquired(self):
+    def test_it_should_renew_the_lock_while_still_being_used(self):
         self.lock.acquire()
-        def acquire_again():
-            print "in the other thread"
-            self.otherLock = Lock(self.client, self.lock.key)
-            self.otherLock.acquire()
-
-        Thread(target=acquire_again).run()
         time.sleep(2)
-        expect(self.otherLock).to.not_eq(None)
-        expect(hasattr(self.otherLock, 'token')).to.be.false()
+        expect(self.client.get(self.lock.key).value).to.eq(self.lock.token)
 
-    def test_it_should_refresh_the_lock_while_still_being_used(self):
-        pass
+    def test_it_cancel_the_renew_after_releasing(self):
+        self.lock.acquire()
+        time.sleep(1)
+        self.lock.release()
+        time.sleep(3)
+        expect(self.client.get(self.lock.key).value).to.eq("0")
 
     def test_it_should_be_usable_as_a_context_manager(self):
-        with self.lock as acquired_lock:
-            print "Aquired!"
+        with self.lock:
+            expect(self.client.get(self.lock.key).value).to.eq(self.lock.token)
+
+        expect(self.client.get(self.lock.key).value).to.eq("0")
+
+
+class MultipleLocksTests(unittest.TestCase):
+    def setUp(self):
+        self.client = etcd.Client()
+        self.key = "lock-%d" % random.uniform(1,100000)
+        self.locks = [
+            Lock(etcd.Client(), self.key, ttl=2, renewSecondsPrior=1, name="A"),
+            Lock(etcd.Client(), self.key, ttl=3, renewSecondsPrior=1, name="B"),
+            Lock(etcd.Client(), self.key, ttl=2, renewSecondsPrior=None, name="C")
+        ]
+
+    def teardown(self):
+        for lock in self.Locks:
+            if (lock != None and lock.token != None):
+                lock.release()
+
+    def test_it_should_not_aquire_a_lock_already_acquired(self):
+        expect(self.locks[0].acquire()).to.be.true()
+        expect(self.locks[1].acquire(timeout=1)).to.be.false()
+        expect(self.locks[1].is_locked()).to.be.false()
+
+    def test_it_should_acquire_the_lock_when_another_agent_releases_it(self):
+        expect(self.locks[0].acquire()).to.be.true()
+
+        t = Thread(target=lambda: expect(self.locks[1].acquire()).to.be.true())
+        t.start()
+        time.sleep(1)
+        self.locks[0].release()
+        t.join()
+
+        expect(self.locks[1].is_locked()).to.be.true()
+
+
