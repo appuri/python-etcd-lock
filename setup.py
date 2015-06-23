4167295b4951541fa47a577f68c0b363e1f39213
Nathan Black <nathan@nathanblack.org>
2015-06-23 13:42:37 -0700

    Initial package setup script

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+version = '0.0.2'
+
+install_requires = [
+    'python-etcd'
+]
+
+test_requires = [
+    'nose',
+    'robber'
+]
+
+setup(name='python-etcd-lock',
+    version=version,
+    description="A distributed lock recipe for etcd",
+    long_description=README,
+    classifiers=[
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database :: Front-Ends",
+    ],
+    keywords='etcd distributed lock semaphore mutex',
+    author='Nathan Black',
+    author_email='nathan@appuri.com',
+    url='https://github.com/appuri/python-etcd-lock',
+    license='ISC',
+    packages=find_packages(''),
+    package_dir = {'': '.'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=test_requires,
+    test_suite='nose.collector',
+
+)
